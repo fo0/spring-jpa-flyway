9755a515539cc3837774aba7b8e545a1d6cdc57f
fo0 <fo0@fo0.me>
2022-05-18 23:36:02 +0200

    refactor

--- src/main/java/com/example/springflyway/data/PersonDto.java
+++ src/main/java/com/example/springflyway/data/PersonDto.java
@@ -1,8 +1,6 @@
 package com.example.springflyway.data;
 
 import java.io.Serializable;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 public record PersonDto(int id, String name, int age, String lastName) implements Serializable {
 
